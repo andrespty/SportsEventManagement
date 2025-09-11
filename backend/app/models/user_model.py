393b92d9b09e20083b89aaffbb22be4c7466f9f9
Andres Ho <andresholee@gmail.com>
2025-09-10 23:21:08 -0400

    Changed password lengths

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -11,8 +11,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), default=UserRole.USER, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     isOwner = db.Column(db.Boolean, default=False, nullable=False)
