91046694dcaf52203f016e703a5bd8de091d23e4
Andres Ho <andresholee@gmail.com>
2025-09-10 22:58:59 -0400

    Changes for db

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
+import pymysql
 
+pymysql.install_as_MySQLdb()
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
